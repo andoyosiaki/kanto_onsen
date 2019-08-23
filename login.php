abd57d74dd6edc23efa0e9fea065df7fb5566e50
andouyosiaki <yutori4dayo@gmail.com>
2019-08-23 16:28:26 +0900

    修正

--- login.php
+++ login.php
@@ -38,6 +38,7 @@ if(!empty($_POST['name']) && !empty($_POST['password'])){ //何かしらの情�
 	<div class="login_form">
 		<form class="" action="" method="post">
 			<p>管理人専用</p>
+			<p>ID:test <br>PASS:test</p>
 			<input type="text" name="name" value="" placeholder="adress"><br>
 			<input type="password" name="password" value="" placeholder="password"><br>
 			<input type="submit" value="送信">
