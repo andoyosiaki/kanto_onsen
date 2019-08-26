eda9a876e10d79ffcad916edafef1ff01ecf088a
andouyosiaki <yutori4dayo@gmail.com>
2019-08-26 14:17:22 +0900

    修正

--- core/dbconect.php
+++ core/dbconect.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-  $db = new PDO('mysql:dbname=onsen;host=localhost;charset=utf8','root', 'root');
+  $db = new PDO('mysql:dbname=onsens;host=localhost;charset=utf8','root', 'root');
 }catch (PDOExeception $e){
   echo "DB接続エラー:". $e->getMassage();
 }
